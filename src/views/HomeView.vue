dda222cfc3fd78f5cf64181cb45f167ad49b1fd8
Dean Alif Ahmad <deanalifahmad49@gmail.com>
2023-12-07 18:30:06 +0700

    fix height styling

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,7 @@
 <template>
     <main>
         <section class="h-100 w-100"
-            style="box-sizing: border-box; background-image: linear-gradient(to bottom, rgba(31, 29, 43, 1), rgba(39, 37, 53, 1));">
+            style="min-height: 800px; box-sizing: border-box; background-image: linear-gradient(to bottom, rgba(31, 29, 43, 1), rgba(39, 37, 53, 1));">
             <div class="content" style="font-family: 'Poppins', sans-serif;">
                 <div class="mx-auto d-flex align-items-center justify-content-center flex-column">
                     <div class="text-center w-100">
